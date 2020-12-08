67a8adf58b01fdac9f6c43006014c75957569221
Mantis-3DP <mantis3dp@posteo.de>
2020-12-08 17:25:20 +0100

    added requirements.txt

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ plugin_url = "https://github.com/Mantis-3DP/Octoprint-Tpc"
 plugin_license = "AGPLv3"
 
 # Any additional requirements besides OctoPrint should be listed here
-plugin_requires = []
+plugin_requires = [numpy, opencv-python]
 
 ### --------------------------------------------------------------------------------------------------------------------
 ### More advanced options that you usually shouldn't have to touch follow after this point
